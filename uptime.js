4d844402658664f4bc1838ba92a71969812bd0ef
Kevin621 <60020468+Kevin621@users.noreply.github.com>
2020-09-10 01:10:15 -0400

    uptime update

--- uptime.js
+++ uptime.js
@@ -2,19 +2,16 @@
 /**
  * @returns {string}
  */
-function time() {
-    let mseconds = Number(process.uptime().toFixed(0)) * 1000;
-    let seconds = Math.floor(mseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
-    let timestring = "";
-
-    if (days > 0) timestring += "Days: " + days;
-    else if (hours > 0) timestring += "Hours: " + hours;
-    else if (minutes > 0) timestring += "Minutes: " + minutes;
-    else if (seconds > 0) timestring += "Seconds: " + seconds;
-
-    return timestring;
+function time(ms, s, m, h, d) {
+    let msec = Number(process.uptime().toFixed(0)) * 1000;
+    let totalSeconds = Math.floor(msec / 1000);
+    let seconds = Math.floor(totalSeconds % 60)
+    let minutes = Math.floor(totalSeconds / 60);
+    totalSeconds %= 3600;
+    let hours = Math.floor(totalSeconds / 3600);
+    let days = Math.floor(totalSeconds / 864000);
+    
+    return(`Days: ${days} Hours: ${hours} Minutes: ${minutes} Seconds: ${seconds}`);
 };
 module.exports = time
+
