bca74495be9624c27dd17748478205261d653c73
Kevin621 <60020468+Kevin621@users.noreply.github.com>
2020-09-14 20:44:49 -0400

    Uptime.js Patch

--- uptime.js
+++ uptime.js
@@ -5,11 +5,11 @@
 function time() {
     let msec = Number(process.uptime().toFixed(0)) * 1000;
     let totalSeconds = Math.floor(msec / 1000);
-    let seconds = Math.floor(totalSeconds % 60)
-    let minutes = Math.floor(totalSeconds / 60);
-    totalSeconds %= 3600;
+    let days = Math.floor(totalSeconds / 86400);
     let hours = Math.floor(totalSeconds / 3600);
-    let days = Math.floor(totalSeconds / 864000);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
     
     return(`Days: ${days} Hours: ${hours} Minutes: ${minutes} Seconds: ${seconds}`);
 };
